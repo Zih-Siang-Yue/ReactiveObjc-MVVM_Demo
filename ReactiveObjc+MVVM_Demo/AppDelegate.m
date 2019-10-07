8afbc879515d0cf265b9b266e6c2a99533bab0cc
Sean Yue <sean.yue@skylinetw.com>
2019-10-07 16:22:50 +0800

    Cuz the 'ViewController' file is unnecessary,  so it had been removed. Making 'LoginVC' be UIWindow its rootViewController.

--- ReactiveObjc+MVVM_Demo/AppDelegate.m
+++ ReactiveObjc+MVVM_Demo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "LoginViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,14 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    LoginViewModel *vm = [LoginViewModel new];
+    LoginViewController *vc = [[LoginViewController alloc] initWithViewModel:vm];
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.rootViewController = nav;
+    [self.window makeKeyAndVisible];
+
     return YES;
 }
 
